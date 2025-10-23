59fb795b8cd265e983b83febb26be1510a3fbfaf
wonwonwonwonwon <hangae31@gmail.com>
2025-10-24 08:34:13 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     // !!! 중요: 배포된 자신의 Google Apps Script 웹 앱 URL
-    const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxBjb_K5lystmdmbP5UBypfSBQ--4bFApKtWxAIrdLpCTTdi-dJB4TU_0clQEMke3Hi/exec'; // <--- 본인 URL 확인
+    const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbz3uFj8xer7h-RyRFmIc6Pwwh3WPtENZERaqD1udNc9WivhHxpseXJQBsdfbgY54B6Z/exec'; // <--- 본인 URL 확인
 
     const recordForm = document.getElementById('record-form');
     const recordsContainer = document.getElementById('records-container');
